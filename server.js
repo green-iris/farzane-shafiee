c6f268accb58a63696c647c0bf1f0f6d40beb32e
iris <lilylotus68@gmail.com>
2018-12-24 17:11:46 +0330

    const port is added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.use('/' , (req , res) => {
     res.send('hello');
 });
 
-app.listen(3000 , () => {
-    console.log('server in runing 3000');
+app.listen(port , () => {
+    console.log(`server in runing ${port}`);
 });
